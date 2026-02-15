51f6ac6bd07be18e723e65a4fa54f1e45504eef5
Ritesh Patel <ritesh@riteshpripatel.com>
2026-02-15 07:18:33 -0700

    Download parquet directly

--- src/medicaid_spending/config.py
+++ src/medicaid_spending/config.py
@@ -2,9 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parents[2]
 
-RAW_DATA_DIR = BASE_DIR / "data" / "raw"
 PROCESSED_DATA_DIR = BASE_DIR / "data" / "processed"
 
-RAW_CSV_PATH = RAW_DATA_DIR / "medicaid-provider-spending.csv"
-PARQUET_PATH = PROCESSED_DATA_DIR / "medicaid_provider_spending.parquet"
+PARQUET_PATH = PROCESSED_DATA_DIR / "medicaid-provider-spending.parquet"
 
--- src/medicaid_spending/download.py
+++ src/medicaid_spending/download.py
@@ -1,15 +1,16 @@
 import requests
-from medicaid_spending.config import RAW_CSV_PATH
+from medicaid_spending.config import PARQUET_PATH
 
-DATASET_URL = "https://stopendataprod.blob.core.windows.net/datasets/medicaid-provider-spending/2026-02-09/medicaid-provider-spending.csv"
+#https://opendata.hhs.gov/datasets/medicaid-provider-spending/
+DATASET_URL = "https://stopendataprod.blob.core.windows.net/datasets/medicaid-provider-spending/2026-02-09/medicaid-provider-spending.parquet"
 
 def download_dataset():
     response = requests.get(DATASET_URL, stream=True)
     response.raise_for_status()  # Ensure we notice bad responses
-    with open(RAW_CSV_PATH, "wb") as f:
+    with open(PARQUET_PATH, "wb") as f:
         for chunk in response.iter_content(chunk_size=8192):
             f.write(chunk)
-    print(f"Dataset downloaded to {RAW_CSV_PATH}")
+    print(f"Dataset downloaded to {PARQUET_PATH}")
 
 if __name__ == "__main__":
     download_dataset()
